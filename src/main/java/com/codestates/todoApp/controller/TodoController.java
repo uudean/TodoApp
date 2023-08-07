c527af12bf8570a7d47378016c3429cfaea2b14d
uudean <swj4545@naver.com>
2023-08-07 22:46:17 +0900

    Update TodoController

--- src/main/java/com/codestates/todoApp/controller/TodoController.java
+++ src/main/java/com/codestates/todoApp/controller/TodoController.java
@@ -1,4 +1,4 @@
-package com.codestates.todoApp;
+package com.codestates.todoApp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;

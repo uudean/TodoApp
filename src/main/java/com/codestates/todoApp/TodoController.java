42114565fed1ca5cfdabc6101dabfff9e9c1a2e9
uudean <swj4545@naver.com>
2023-08-07 21:52:07 +0900

    first

---
+++ src/main/java/com/codestates/todoApp/TodoController.java
@@ -0,0 +1,12 @@
+package com.codestates.todoApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+    @GetMapping("/")
+    public String todo() {
+        return "To-do Application !";
+    }
+}
